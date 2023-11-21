93fb96d386f09b27ebd9be14ba7302ab8b7c398e
h0ngwon <w___on@naver.com>
2023-11-21 20:52:43 +0900

    Rename: App.js 확장자명 변경 to App.jsx

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -6,7 +6,7 @@ const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={Home} />
+                <Route path="/" element={<Home />} />
             </Routes>
         </BrowserRouter>
     );
