ca5b2bb85903413ad47c7114b4f2b225b702c2b5
h0ngwon <w___on@naver.com>
2023-11-21 19:27:54 +0900

    Chore: 파이어베이스 세팅 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import { firebaseApp } from './shared/firebase';
 
 const Test = styled.div`
     background-color : ${(props) => props.theme.mainColor};
 `
+
+console.log(firebaseApp);
 
 const App = () => {
     return <Test>App</Test>;
