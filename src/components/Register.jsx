2b2ff6e6534292d548165208474240c5e08f13e2
h0ngwon <w___on@naver.com>
2023-11-22 15:05:20 +0900

    Rename: pages 파일 이름 변경

---
+++ src/components/Register.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Header = styled.header`
+    width: 100%;
+    text-align: center;
+    font-size: 36px;
+`;
+
+const Register = () => {
+    return (
+        <>
+            <Header>회원가입</Header>
+        </>
+    );
+};
+
+export default Register;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
 import Layout from '../components/Layout';
+import RegisterPage from '../pages/RegisterPage';
+import HomePage from '../pages/HomePage';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<Layout />}>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="register" element={<RegisterPage/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
