3ace63b2fb2276beb019d07d22404844beb9202f
heeneeee <gmldnjd0614@gmail.com>
2023-11-24 14:30:46 +0900

    Fix : 코드 수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,23 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import defaultImage from '../assets/default.jpeg';
-import sampleUserinfo from '../shared/sampleUserinfo.json';
-import samplePost from '../shared/samplePost.json';
 import { db } from '../shared/firebase';
-import { collection, getDocs, query } from 'firebase/firestore';
+import { collection, getDocs, query, orderBy } from 'firebase/firestore';
+import { useNavigate } from 'react-router';
 
 export default function MyPage() {
-    const [userInfo, setUserInfo] = useState(sampleUserinfo);
-    const [posts, setPosts] = useState();
+    const [name, setName] = useState('');
+
+    const [posts, setPosts] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
-            // collection 이름이 samplePost인 collection의 모든 document를 가져옴
-
-            const q = query(collection(db, 'samplePost'));
-
+            // collection 이름이 post인 collection의 모든 document를 가져옴
+            const q = query(collection(db, 'post'), orderBy('timestamp', 'desc'));
             const querySnapshot = await getDocs(q);
-
             const fbdata = querySnapshot.docs.map((doc) => doc.data());
             setPosts(fbdata);
         };
@@ -25,37 +23,79 @@ export default function MyPage() {
         fetchData();
     }, []);
     console.log(posts);
+
+    // post 삭제 기능
+    const deletePost = (post) => {
+        const deleted = posts.filter((data) => {
+            return data.id !== post.id;
+        });
+        setPosts(deleted);
+    };
+
     return (
         <>
             <Header>
+                {/* 로고 만들어서 home으로 이동하기 */}
                 <Title>마이페이지</Title>
             </Header>
             <ProfileEdit>
                 <MyPhoto>
+                    {/* 로그인한 user의 photo 불러오기 */}
                     <img src={defaultImage} />
                 </MyPhoto>
                 <UserEdit>
+                    {/* 로그인한 user의 displayname, 소개글 불러오기 */}
+                    {/* 비동기 처리하면 됨 */}
                     <Ment>박희원님, 반갑습니다!</Ment>
-                    <EditBtn>프로필 수정</EditBtn>
+                    <EditBtn
+                        onClick={() => {
+                            navigate(`/modal`);
+                        }}
+                    >
+                        프로필 수정
+                    </EditBtn>
                 </UserEdit>
             </ProfileEdit>
             <MyPost>나의 게시물</MyPost>
             <PostContainer>
-                {/* 컴포넌트 PostList */}
+                {/* PostList.jsx 컴포넌트 생성 */}
                 <PostList>
-                    {samplePost.map((post) => {
+                    {posts.map((post) => {
                         return (
-                            // 컴포넌트 post
-                            <Post key={post.id}>
+                            // Post.jsx 컴포넌트 생성
+                            <Post key={post.timestamp}>
                                 <div>
-                                    <PostImage src={post.image} alt='이미지' />
+                                    {/* 이미지 누르면 상세페이지로 이동 구현 */}
+                                    {/* post이미지 60개 객체 나오는 거 해결하기 */}
+                                    <PostImage
+                                        onClick={() => {
+                                            navigate(`/detailpage/${post.id}`);
+                                        }}
+                                        src={post.image}
+                                        alt='이미지'
+                                    />
                                 </div>
                                 <PostTitle>{post.title}</PostTitle>
                                 <PostComment>{post.comment}</PostComment>
                                 <Buttons>
+                                    {/* user에 따라 좋아요 눌린 값 가져오기 */}
                                     <div>♥️</div>
-                                    <Button>수정</Button>
-                                    <Button>삭제</Button>
+                                    <Button
+                                        onClick={() => {
+                                            navigate(`/editDetail`);
+                                        }}
+                                    >
+                                        수정
+                                    </Button>
+                                    {/* 삭제하시겠습니까? alret창 띄우기 네 or 아니오 */}
+                                    {/* 삭제 기능 리덕스로 구현해보기 */}
+                                    <Button
+                                        onClick={() => {
+                                            deletePost(post);
+                                        }}
+                                    >
+                                        삭제
+                                    </Button>
                                 </Buttons>
                             </Post>
                         );
@@ -75,7 +115,7 @@ const Header = styled.div`
 
 const Title = styled.h1`
     margin: 30px 30px;
-    font-size: 20px;
+    font-size: 30px;
     font-weight: 500;
 `;
 
@@ -129,8 +169,9 @@ const MyPost = styled.h2`
     display: block;
     padding-top: 50px;
     padding-bottom: 70px;
-    padding-left: 70px;
-    font-size: 20px;
+    padding-left: 100px;
+    font-size: 25px;
+    font-weight: 500;
     border-top: 2px solid lightgrey;
 `;
 
@@ -165,6 +206,9 @@ const Button = styled.button`
     border: 0px;
     margin-bottom: 30px;
     cursor: pointer;
+    &:hover {
+        transform: scale(1.1);
+    }
 `;
 
 const PostImage = styled.img`
@@ -176,7 +220,7 @@ const PostImage = styled.img`
 `;
 
 const PostTitle = styled.p`
-    height: 50px;
+    height: 100px;
 `;
 
 const PostComment = styled.p`
