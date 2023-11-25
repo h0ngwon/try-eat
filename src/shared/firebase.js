3271d4342e957d330fcb3b1c45a5c98b06340426
h0ngwon <w___on@naver.com>
2023-11-25 14:16:37 +0900

    Chore: 프로젝스 세팅 변경

--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -9,12 +9,12 @@ import { updateProfile } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAitIVUmb2UlT4AoxvPfwMtw0ZuloGyWxo",
-    authDomain: "pleasebe-1cfe1.firebaseapp.com",
-    projectId: "pleasebe-1cfe1",
-    storageBucket: "pleasebe-1cfe1.appspot.com",
-    messagingSenderId: "805807962626",
-    appId: "1:805807962626:web:9184537cd6b06e506d451b"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
